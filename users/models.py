af3319f18bf035965267c47a4bd06068790073be
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-22 21:57:56 +0530

    Removed

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class User(models.Model):
-    username = models.CharField(max_length=40)
-    password = models.CharField(max_length=40)
-    email = models.CharField(max_length=40)
