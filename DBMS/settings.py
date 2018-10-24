95d6d3e040d8e9b6ef697d830252a099505faaef
GauriBaraskar <gauribaraskar@gauribaraskar-Inspiron-15-3567>
2018-10-24 14:52:42 +0530

    Fixed dashboard

--- DBMS/settings.py
+++ DBMS/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'DBMS',
         'USER': 'root',
-        'PASSWORD': '@yu$h113'
+        'PASSWORD': 'chibbu'
     }
 }
 
