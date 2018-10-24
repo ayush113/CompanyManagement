688718032dabf7b9e1022a45d6c8f30df1e1811d
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-24 18:30:21 +0530

    Templates for data fetch added

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+from projects.models import Project
+from django.db import connection
+from .utils import namedtuplefetchall
 
 @login_required
 def projects(request):
-
-    return render(request,'projects/index.html')
+    with connection.cursor() as curr:
+        curr.execute("SELECT project.project_id,project_name,leader,customer_id,start_date,"
+                     "deadline FROM project INNER JOIN works_on ON project.project_id=works_on.project_id "
+                     "WHERE works_on.user_id=%s ",[request.user.id])
+        res = namedtuplefetchall(curr)
+        print(res[0].start_date)
+    return render(request,'projects/index.html',{'projects':res})
