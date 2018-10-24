04b174412c5d3c73dbf5392ad3ce50e38f78e7c4
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-24 17:46:05 +0530

    Models Added

--- custsupport/models.py
+++ custsupport/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Customer(models.Model):
+    customer_id = models.IntegerField(primary_key=True)
+    complaints = models.TextField(blank=True, null=True)
+    bills = models.TextField(blank=True, null=True)
+    changes = models.TextField(blank=True, null=True)
+    contact = models.TextField(blank=True, null=True)
+    point_of_contact = models.ForeignKey(User, models.DO_NOTHING, db_column='point_of_contact', blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'customer'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from custsupport.models import Customer
 
 # Create your models here.
+
+class Project(models.Model):
+    project_id = models.IntegerField(primary_key=True)
+    project_name = models.CharField(max_length=40, blank=True, null=True)
+    leader = models.ForeignKey(User, models.DO_NOTHING, db_column='leader', blank=True, null=True)
+    customer = models.ForeignKey(Customer, models.DO_NOTHING, blank=True, null=True)
+    start_date = models.DateField(blank=True, null=True)
+    deadline = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'project'
