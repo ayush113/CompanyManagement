63c08a473be57c187f87c4124fc20d4b1fcedb82
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-24 22:23:58 +0530

    Projects App Added

--- projects/views.py
+++ projects/views.py
@@ -8,9 +8,6 @@ from .utils import namedtuplefetchall
 @login_required
 def projects(request):
     with connection.cursor() as curr:
-        curr.execute("SELECT project.project_id,project_name,leader,customer_id,start_date,"
-                     "deadline FROM project INNER JOIN works_on ON project.project_id=works_on.project_id "
-                     "WHERE works_on.user_id=%s ",[request.user.id])
+        curr.execute("select first_name,last_name,q1.project_id,project_name,leader,customer_id,start_date,deadline from auth_user,(SELECT project.project_id,project_name,leader,customer_id,start_date,deadline FROM project INNER JOIN works_on ON project.project_id=works_on.project_id WHERE works_on.user_id=%s) as q1 where q1.leader = auth_user.id",[request.user.id])
         res = namedtuplefetchall(curr)
-        print(res[0].start_date)
     return render(request,'projects/index.html',{'projects':res})
