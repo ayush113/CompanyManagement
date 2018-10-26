fc4cb4afca66dd85ea36983acc9bdd21e15256c3
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-26 17:04:56 +0530

    Some Changes

--- home/views.py
+++ home/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 
 from django.contrib.auth.decorators import login_required
 
+from django.db import connection
+
+from projects.utils import namedtuplefetchall
+
 # Create your views here.
 def landing(request):
     return render(request,'home/landing.html')
 
 @login_required
 def dashboard(request):
-    return render(request,'home/dashboard.html')
+    with connection.cursor() as curr:
+        curr.execute("SELECT * FROM reminder WHERE id = %s",[request.user.id])
+        res = namedtuplefetchall(curr)
+    return render(request,'home/dashboard.html',{'result': res})
