ba7cf83a66e2c02c05bc833a10cd610912f3b67a
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-27 22:50:46 +0530

    CSRF Token Error Fixed

--- leads/views.py
+++ leads/views.py
@@ -8,30 +8,46 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 @login_required
-def leads(request):
-    with connection.cursor() as curr:
-        curr.execute("select leads.customer_id,customer_name,proposed_project_info,time_required,cost,leads.info from customer,leads where point_of_contact=%s and leads.customer_id=customer.customer_id",[request.user.id])
-        res = namedtuplefetchall(curr)
-    return render(request,'leads/index.html',{'leads':res})
-
-@login_required
 @csrf_exempt
-def editpage(request):
+def leads(request):
     if request.method == "POST":
         data = request.POST
+        print(data)
+
+        # Add your code here
+
+        # make sure data formats are right
+
+        return JsonResponse(1,safe=False)
+    else:
+        with connection.cursor() as curr:
+            curr.execute(
+                "select leads.customer_id,customer_name,proposed_project_info,time_required,cost,leads.info from customer,leads where point_of_contact=%s and leads.customer_id=customer.customer_id",
+                [request.user.id])
+            res = namedtuplefetchall(curr)
+        return render(request, 'leads/index.html', {'leads': res})
 
 
-        print('sdfghj')
 
-        name = data.get('name')
-        name = json.loads(name)
-        customerID = data.get('customerID')
-        customerID = json.loads(customerID)
-        descript = data.get('descript')
-        descript = json.loads(descript)
-        cost = data.get('cost')
-        cost = json.loads(cost)
 
-        print(name)
-
-        return JsonResponse(1, safe=False)
+# @login_required
+# @csrf_exempt
+# def editpage(request):
+#     if request.method == "POST":
+#         data = request.POST
+#
+#
+#         print('sdfghj')
+#
+#         name = data.get('name')
+#         name = json.loads(name)
+#         customerID = data.get('customerID')
+#         customerID = json.loads(customerID)
+#         descript = data.get('descript')
+#         descript = json.loads(descript)
+#         cost = data.get('cost')
+#         cost = json.loads(cost)
+#
+#         print(name)
+#
+#         return JsonResponse(1, safe=False)
