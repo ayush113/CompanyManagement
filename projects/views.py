ed73cd93361958d38a278263071ee27d605cb586
ayush113 <ayush.kumar1999@hotmail.com>
2018-10-27 17:50:44 +0530

    Intermediate Changes

--- projects/views.py
+++ projects/views.py
@@ -14,6 +14,7 @@ def projects(request):
     with connection.cursor() as curr:
         curr.execute("select first_name,last_name,q1.project_id,project_name,leader,customer_id,start_date,deadline from auth_user,(SELECT project.project_id,project_name,leader,customer_id,start_date,deadline FROM project INNER JOIN works_on ON project.project_id=works_on.project_id WHERE works_on.user_id=%s) as q1 where q1.leader = auth_user.id",[request.user.id])
         res = namedtuplefetchall(curr)
+
     return render(request,'projects/index.html',{'projects':res})
 
 @login_required
@@ -56,7 +57,7 @@ def editpage(request):
             res = namedtuplefetchall(curr)
 
         with connection.cursor() as curr:
-            curr.execute("select first_name,last_name,role,works_on.user_id from works_on,auth_user where works_on.user_id = auth_user.id and works_on.project_id=%s",[request.GET.get('project_id')])
+            curr.execute("select first_name,last_name,role,works_on.user_id,auth_user.email from works_on,auth_user where works_on.user_id = auth_user.id and works_on.project_id=%s",[request.GET.get('project_id')])
             roles = namedtuplefetchall(curr)
         try:
             return render(request, 'projects/project.html', {'data': res[0],'roles':roles,'leader':leader})
